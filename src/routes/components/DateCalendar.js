37cb278494e4e519622b23132950f5e8660dbfb1
Seo <kirin2211@naver.com>
2023-03-20 00:05:53 +0900

    full-calendar

--- src/routes/components/DateCalendar.js
+++ src/routes/components/DateCalendar.js
@@ -1,101 +1,84 @@
 import React, { useState } from "react";
-import Calendar from "react-calendar";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import bootstrapPlugin from "@fullcalendar/bootstrap";
+import interactionPlugin from "@fullcalendar/interaction";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+} from "@mui/material";
+// import { GithubPicker } from "react-color";
 import "./css/Calendar.css";
-import AddScheduleModal from "./elements/ScheduleModal";
-import Modal from "react-modal";
 
-import Zoom from "@mui/material/Zoom";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
+function getRandomColor() {
+  const colors = [
+    "#b80000",
+    "#db3e00",
+    "#fccb00",
+    "#008b02",
+    "#006b76",
+    "#1273de",
+    "#004dcf",
+    "#5300eb",
+    "#eb9694",
+    "#fad0c3",
+    "#fef3bd",
+    "#c1e1c5",
+    "#bedadc",
+    "#c4def6",
+    "#bed3f3",
+    "#d4c4fb",
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Zoom timeout={1000} ref={ref} {...props} />;
-});
-
-function CustomCalendar() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
-  //이미 있는 스케줄을 보여주는 모달
-  const [showEvent, setShowEvent] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState({});
-
-  //스케줄 추가 기능 구현을 위한 모달
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [schedule, setSchedule] = useState({ title: "", description: "" });
-
+function App() {
   const [events, setEvents] = useState([]);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [newEvent, setNewEvent] = useState({});
+  const [profilecolor, setProfilecolor] = useState("#fff");
 
-  const onClickDay = (value) => {
-    const selectedDate = value.toISOString().slice(0, 10);
-    const event = events.find((event) => event.date === selectedDate);
-    console.log(event);
-    if (event) {
-      setSelectedEvent(event);
-      setShowEvent(true);
-    } else {
-      setShowEvent(false);
-    }
+  const handleDateSelect = (arg) => {
+    console.log(arg);
+    setNewEvent({
+      title: "",
+      start: arg.startStr,
+      end: arg.endStr,
+      color: getRandomColor(),
+    });
+    setDialogOpen(true);
   };
 
-  const onCloseEvent = () => {
-    setShowEvent(false);
+  const handleDialogClose = () => {
+    setNewEvent({});
+    setDialogOpen(false);
   };
 
-  const tileContent = ({ date, view }) => {
-    const eventDates = events.map((event) => event.date);
-    const eventCount = eventDates.filter(
-      (eventDate) => eventDate === date.toISOString().slice(0, 10)
-    ).length;
-    return eventCount > 0 && view === "month" && <div className="dot"></div>;
-  };
-
-  const eventModal = (
-    <div className="event-modal">
-      <h2>{selectedEvent.title}</h2>
-      <p>{selectedEvent.content}</p>
-      <button onClick={onCloseEvent}>Close</button>
-    </div>
-  );
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
-  const handleTitleChange = (e) => {
-    setSchedule((prevSchedule) => ({
-      ...prevSchedule,
-      title: e.target.value,
-    }));
-    console.log(schedule);
-  };
-
-  const handleDescriptionChange = (e) => {
-    setSchedule((prevSchedule) => ({
-      ...prevSchedule,
-      description: e.target.value,
-    }));
-    console.log(schedule);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setEvents((prevEvent) => [
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewEvent((prevEvent) => ({
       ...prevEvent,
-      {
-        date: selectedDate.toISOString().slice(0, 10),
-        title: schedule.title,
-        content: schedule.description,
-      },
-    ]);
+      [name]: value,
+    }));
+  };
 
-    setModalIsOpen(false);
+  const handleEventRender = (info) => {
+    info.el.style.backgroundColor = info.event.extendedProps.color;
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+    handleDialogClose();
+  };
+
+  const handleChangeComplete = (color, event) => {
+    setProfilecolor(color.hex);
   };
 
   return (
@@ -106,47 +89,62 @@ function CustomCalendar() {
           display: "flex",
           flexDirection: "row",
         }}
-      >
-        <Calendar
-          value={selectedDate}
-          onClickDay={onClickDay}
-          tileContent={tileContent}
-          onChange={setSelectedDate}
-          next2Label={null}
-          prev2Label={null}
-        />
-        <div>
-          {" "}
-          <button onClick={openModal}>스케줄 추가</button>
-          <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-            <h2>스케줄 추가</h2>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="title">제목</label>
-                <input
-                  type="text"
-                  id="title"
-                  value={schedule.title}
-                  onChange={handleTitleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="description">내용</label>
-                <textarea
-                  id="description"
-                  value={schedule.description}
-                  onChange={handleDescriptionChange}
-                />
-              </div>
-              <button type="submit">추가</button>
-              <button onClick={closeModal}>닫기</button>
-            </form>
-          </Modal>
-        </div>
-      </div>
-      {showEvent && eventModal}
+      ></div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable={true}
+        select={handleDateSelect}
+        events={events}
+        eventRender={handleEventRender}
+      />
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>일정 추가</DialogTitle>
+        <form onSubmit={handleFormSubmit}>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="일정 제목"
+              type="text"
+              fullWidth
+              name="title"
+              value={newEvent.title}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="시작 시간"
+              type="datetime-local"
+              fullWidth
+              name="start"
+              value={newEvent.start}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="종료 시간"
+              type="datetime-local"
+              fullWidth
+              name="end"
+              value={newEvent.end}
+              onChange={handleInputChange}
+            />
+            {/* <GithubPicker
+              triangle="hide"
+              onChangeComplete={handleChangeComplete}
+            /> */}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose}>취소</Button>
+            <Button type="submit" disabled={!newEvent.title}>
+              추가
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </div>
   );
 }
 
-export default CustomCalendar;
+export default App;
