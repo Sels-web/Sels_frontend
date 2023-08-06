37ea48920b2163ce266543da7bd36fc758868564
박총명 <qkrri56@naver.com>
2023-08-06 17:13:33 +0900

    [ENH] Change Home button

--- src/_nav.js
+++ src/_nav.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
-import { cilCalendar, cilPeople, cilSave, cilSpeedometer } from '@coreui/icons'
+import { cilCalendar, cilPeople, cilSave, cilHome } from '@coreui/icons'
 import { CNavItem, CNavTitle } from '@coreui/react'
 
 const _nav = [
   {
     component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      color: 'info',
-      text: 'NEW',
-    },
+    name: 'Home',
+    to: '/Home',
+    icon: <CIcon icon={cilHome} customClassName="nav-icon" />,
   },
   {
     component: CNavTitle,
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -29,22 +29,6 @@ const AppHeader = () => {
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
-        <CHeaderBrand className="mx-auto d-md-none" to="/">
-          <CIcon icon={""} height={48} alt="Logo" />
-        </CHeaderBrand>
-        <CHeaderNav className="d-none d-md-flex me-auto">
-          <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink}>
-              Dashboard
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
-          </CNavItem>
-        </CHeaderNav>
       </CContainer>
       <CHeaderDivider />
       <CContainer fluid>
