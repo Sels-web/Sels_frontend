acaf5f18bed5954b8c1152cd72f21c2c556b4ffb
박총명 <qkrri56@naver.com>
2023-08-06 17:21:27 +0900

    [ENH] Rename routes

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ const Admin = React.lazy(() => import('./views/admin/page/Admin'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/schedule', name: 'Schedule', element: Schedule },
-  { path: '/members', name: 'Members', element: Members },
-  { path: '/boards', name: 'Boards', element: Boards },
-  { path: '/admin', name: 'Admin', element: Admin },
+  { path: '/schedule', name: '일정', element: Schedule },
+  { path: '/members', name: '명단', element: Members },
+  { path: '/boards', name: '자료실', element: Boards },
+  { path: '/admin', name: '관리자', element: Admin },
 ]
 
 export default routes
