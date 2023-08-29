0e018f51695139a0b680f9ecaa117035929f24a5
박총명 <qkrri56@naver.com>
2023-08-29 13:57:36 +0900

    [ENH] Block boards page

--- src/views/boards/page/Boards.js
+++ src/views/boards/page/Boards.js
@@ -26,30 +26,31 @@ const Boards = () => {
         <CCard>
           <CCardHeader className={'d-flex justify-content-between'}>
             <h3>셀스 자료실</h3>
-            <Link to={'/boards/add'}><CButton color="warning">게시물 등록</CButton></Link>
+            {/*<Link to={'/boards/add'}><CButton color="warning">게시물 등록</CButton></Link>*/}
           </CCardHeader>
           <CCardBody>
-            <CTable hover bordered>
-              <CTableHead align={'center'}>
-                <CTableRow>
-                  <CTableHeaderCell scope="col"></CTableHeaderCell>
-                  <CTableHeaderCell scope="col">제목</CTableHeaderCell>
-                  <CTableHeaderCell scope="col" className={'w-25'}>작성일</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                {items.map((item,idx) => {
-                  return (
-                  <CTableRow align={'middle'} key={idx}>
-                    <CTableHeaderCell className={'text-center'} scope="row">{item.id}</CTableHeaderCell>
-                    <CTableDataCell>
-                      <Link to={'/boards/' + item.id} className={'link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'}>{item.title}</Link>
-                    </CTableDataCell>
-                    <CTableDataCell className={'text-center'}>{item.date}</CTableDataCell>
-                  </CTableRow>
-                  )})}
-              </CTableBody>
-            </CTable>
+            <p>준비중입니다.</p>
+            {/*<CTable hover bordered>*/}
+            {/*  <CTableHead align={'center'}>*/}
+            {/*    <CTableRow>*/}
+            {/*      <CTableHeaderCell scope="col"></CTableHeaderCell>*/}
+            {/*      <CTableHeaderCell scope="col">제목</CTableHeaderCell>*/}
+            {/*      <CTableHeaderCell scope="col" className={'w-25'}>작성일</CTableHeaderCell>*/}
+            {/*    </CTableRow>*/}
+            {/*  </CTableHead>*/}
+            {/*  <CTableBody>*/}
+            {/*    {items.map((item,idx) => {*/}
+            {/*      return (*/}
+            {/*      <CTableRow align={'middle'} key={idx}>*/}
+            {/*        <CTableHeaderCell className={'text-center'} scope="row">{item.id}</CTableHeaderCell>*/}
+            {/*        <CTableDataCell>*/}
+            {/*          <Link to={'/boards/' + item.id} className={'link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'}>{item.title}</Link>*/}
+            {/*        </CTableDataCell>*/}
+            {/*        <CTableDataCell className={'text-center'}>{item.date}</CTableDataCell>*/}
+            {/*      </CTableRow>*/}
+            {/*      )})}*/}
+            {/*  </CTableBody>*/}
+            {/*</CTable>*/}
           </CCardBody>
         </CCard>
       </>
